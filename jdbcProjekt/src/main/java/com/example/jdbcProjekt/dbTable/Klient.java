b18f4dabbd298ec072c9a5eb9425ce4ab6b2f72b
dszczutkowski <dszczutkowski@outlook.com>
2015-11-11 13:15:09 +0100

    Update Klient.java

--- jdbcProjekt/src/main/java/com/example/jdbcProjekt/dbTable/Klient.java
+++ jdbcProjekt/src/main/java/com/example/jdbcProjekt/dbTable/Klient.java
@@ -1,5 +1,56 @@
 package com.example.jdbcProjekt.dbTable;
 
-public class Klient {
+public class Klient
+{
+	private long idKlient;
+	private int nrStolika;
+	private double doZaplaty;
+	private boolean wino;
+	
+	public Klient(int nrStolika, double doZaplaty, boolean wino)
+	{
+		this.nrStolika = nrStolika;
+		this.doZaplaty = doZaplaty;
+		this.wino = wino;
+	}
+	
+	public long getId()
+	{
+		return idKlient;
+	}
+	
+	public void setId(long idKlient)
+	{
+		this.idKlient = idKlient;
+	}
+	
+	public int getNrStolika()
+	{
+		return nrStolika;
+	}
+	
+	public void setNrStolika(int nrStolika)
+	{
+		this.nrStolika = nrStolika;
+	}
+	
+	public double getDoZaplaty()
+	{
+		return doZaplaty;
+	}
 
+	public void setDoZaplaty(double doZaplaty)
+	{
+		this.doZaplaty = doZaplaty;
+	}
+	
+	public boolean getWino()
+	{
+		return wino;
+	}
+	
+	public void setWino(boolean wino)
+	{
+		this.wino = wino;
+	}
 }

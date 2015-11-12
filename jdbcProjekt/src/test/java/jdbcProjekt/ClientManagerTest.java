5c4748c204424b2a88803705f7165288e229f607
dszczutkowski <dszczutkowski@outlook.com>
2015-11-12 12:44:48 +0100

    Add ClientManagerTest

---
+++ jdbcProjekt/src/test/java/jdbcProjekt/ClientManagerTest.java
@@ -0,0 +1,14 @@
+package jdbcProjekt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

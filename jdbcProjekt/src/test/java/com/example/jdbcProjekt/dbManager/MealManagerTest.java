555142c09634d4e11053e323b83a574f810e1ccf
dszczutkowski <dszczutkowski@outlook.com>
2015-11-22 18:35:27 +0100

    dodanie testow do mealManager, naprawa bledow

--- jdbcProjekt/src/test/java/com/example/jdbcProjekt/dbManager/MealManagerTest.java
+++ jdbcProjekt/src/test/java/com/example/jdbcProjekt/dbManager/MealManagerTest.java
@@ -3,7 +3,7 @@ package com.example.jdbcProjekt.dbManager;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
-
+import java.util.List;
 import org.junit.Test;
 
 import com.example.jdbcProjekt.dbTable.Client;
@@ -14,12 +14,55 @@ public class MealManagerTest
 	MealManager mealManager = new MealManager();
 	ClientManager clientManager = new ClientManager();
 	
+	private final static int SEAT_1 = 8;
+	private final static double PAYMENT_1 = 888;
+	private final static boolean WINE_1 = true;
 	
+	private final static String NAME_1 = "Salmon";
+	private final static int AMOUNT_1 = 2; 
+	private final static double PRICE_1 = 35;
+	
+	Client client = new Client(SEAT_1, PAYMENT_1, WINE_1);
 	
 	@Test
 	public void checkConnection() 
 	{
 		assertNotNull(mealManager.getConnection());
+	}
+	
+	@Test
+	
+	public void checkAdding() throws SQLException
+	{
+		clientManager.addClient(client);
+		
+		Meal meal = new Meal(NAME_1, AMOUNT_1, PRICE_1);
+		
+		mealManager.removeMeals();
+		assertEquals(1, mealManager.addMeal(meal));
+		
+		List<Meal>meals = mealManager.getAllMeals();
+		Meal mealRetrieved = meals.get(0);
+		
+		assertEquals(NAME_1, mealRetrieved.getName());
+		assertEquals(AMOUNT_1, mealRetrieved.getAmount());
+		//assertEquals(PRICE_1, mealRetrieved.getPrice());
+	}
+
+	@Test
+	public void checkKeyUpdate() throws SQLException
+	{
+		Meal meal = new Meal(NAME_1, AMOUNT_1, PRICE_1);
+		
+		mealManager.removeMeals();
+		
+		assertEquals(1, mealManager.addMeal(meal));
+		mealManager.setKeyToNull(meal);
+		
+		List<Meal>meals = mealManager.getAllMeals();
+		Meal mealRetrieved = meals.get(0);
+		
+		assertNotNull(mealRetrieved.getIdClient());
 	}
 	
 	@Test
